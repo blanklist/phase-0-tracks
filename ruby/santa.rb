866de0df23ceb085cb4de8d3e69c05b9aa847689
blanklist <joehineriv@gmail.com>
2017-04-10 20:32:47 -0700

    complete all santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,14 +1,16 @@
 class Santa
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
 
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
-    @age = 0
+    @age = rand(0..140)
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     p "Initializing Santa instance ..."
   	p "This santa identifies as #{gender} with an ethnicity of #{ethnicity}."
   end
-
+  
   def speak
   	p "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -26,19 +28,6 @@ class Santa
     @reindeer_ranking.push(bad_reindeer)
   end
 
-  # GETTER METHODS
-  def age
-  	@age
-  end
-
-  def ethnicity
-  	@ethnicity
-  end
-
-  # SETTER METHODS
-  def gender=(new_gender)
-  	@gender = new_gender
-  end
 end
 
 # roger = Santa.new
@@ -46,13 +35,18 @@ end
 # roger.eat_milk_and_cookies("thin mint")
 
 santas = []
-example_genders = ["male", "female", "bi-gender", "gender fluid", "prefer not to state"]
-example_ethnicities = ["prefer not to state", "Caucasian", "African", "Asian", "Latin American"]
+example_genders = ["male", "female", "bi-gender", "gender fluid", "prefer not to state", "genderless", "androgyne", "genderqueer", "hijra", "pangender", "queer hetero"]
+example_ethnicities = ["prefer not to state", "Caucasian", "African", "Asian", "Latin American", "India", "Chinese", "Russian", "Egyptian", "Georgians", "Kurdish", "Nigerian"]
 
 example_genders.length.times { |i| santas << Santa.new(example_genders[i], example_ethnicities[i]) }
 
 p santas[1].celebrate_birthday
-p santas[1].celebrate_birthday
-p santas[1].ethnicity
 
 p santas[0].get_mad_at("Prancer")
+
+p santas[2].gender = example_genders[3]
+
+100.times do |santa|
+	santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+	p "This santa is the ripe ol' age of #{santa.age}"
+end
