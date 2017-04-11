4e43138d13d318faa8d6b9b6738ad9bb78c6f194
blanklist <joehineriv@gmail.com>
2017-04-10 17:50:00 -0700

    complete release0 and release1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,37 @@
 class Puppy
 
+	def initialize
+		puts "Initializing new Puppy instance..."
+	end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(int)
+  	puts "Woof!" * int
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	human_years * 7
+  end
+
+  def play_dead
+  	puts "-silence-"
+  end
+
 end
 
+## DRIVER CODE
+fido = Puppy.new
+fido.fetch("ball")
+
+fido.speak(5)
+fido.roll_over
+p fido.dog_years(3)
+fido.play_dead
