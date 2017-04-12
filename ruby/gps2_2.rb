dd9a4b0cf4af1e9a66eb16b52b7be1124278ca1b
blanklist <joehineriv@gmail.com>
2017-04-12 07:42:22 -0700

    working code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,3 +1,17 @@
+# Method to print a list and make it look pretty
+def print_list(list)
+# input: completed list
+# steps:
+  # iterate over list and print formatted list
+  # format: each item with its own line
+  # "item - quantity"
+  list.each do |item, quantity|
+  	puts "#{item}, qty: #{quantity}"
+  end
+# output: implicit return of list
+  list
+end
+
 # Method to create a list
 def create_list(string_of_items)
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
@@ -9,11 +23,9 @@ def create_list(string_of_items)
   # set default quantity, default quantity of one
   items_in_array.each { |item| items_in_hash[item] = 1 }  
   # print the list to the console [can you use one of your other methods here?]
-  p items_in_hash
-# output: [what data type goes here: hash!]
+  print_list(items_in_hash)
+# output: [what data type goes here: hash]
 end
-
-example_list = create_list("apples bananas cereals avocados")
 
 # Method to add an item to a list
 def add_item(list, item, quantity = 1)
@@ -22,13 +34,9 @@ def add_item(list, item, quantity = 1)
   # check for optional quantity. if not present, set to default (1)
   list[item] = quantity
   # update list with input
-  p list
+  list
 # output: complete list, updated
 end
-
-add_item(example_list, "nutela", 4)
-add_item(example_list, "donuts")
-
 
 # Method to remove an item from the list
 def remove_item(list, item)
@@ -38,38 +46,29 @@ def remove_item(list, item)
   # remove item
   list.delete(item)
   # print success message of item removed
-  p list
+  p "You've removed #{item}."
 # output: updated list
+  list
 end
-
-remove_item(example_list, "bananas")
 
 # Method to update the quantity of an item
 def update_item(list, item, quantity)
 # input: list, item and quantity to be updated to
 # steps:
-  # check if item exists
   # update quantity
   list[item] = quantity
   # print success "your cart has been updated!"
   p "Your cart has been updated!"
-  p list
+  list
 # output: updated list with new quantity
 end
 
-update_item(example_list, "bananas", 100)
 
-# Method to print a list and make it look pretty
-def print_list(list)
-# input: completed list
-# steps:
-  # iterate over list and print formatted list
-  list.each do |item, quantity|
-  	puts "#{item}, qty: #{quantity}"
-  end
-  # format: each item with its own line
-  # "item - quantity"
-# output: implicit return of list
-end
-
+example_list = create_list("")
+add_item(example_list, "Lemondade", 2)
+add_item(example_list, "Tomatoes", 3)
+add_item(example_list, "Onions")
+add_item(example_list, "Ice Cream", 4)
+remove_item(example_list, "Lemondade")
+update_item(example_list, "Ice Cream", 1)
 print_list(example_list)
