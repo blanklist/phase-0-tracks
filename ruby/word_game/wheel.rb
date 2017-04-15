2d51dd944ec9c6800da6a87c9dfbd6fbd27c433c
blanklist <joehineriv@gmail.com>
2017-04-15 14:37:43 -0700

    working logic

--- ruby/word_game/wheel.rb
+++ ruby/word_game/wheel.rb
@@ -9,7 +9,7 @@ class Guess
     # in_progress_word = []
 		@in_progress_word = "_" * correct_word.length
     # number of guesses
-		@number_of_guesses = correct_word.length * 2
+		@number_of_guesses = (correct_word.length * 1.5).to_i
     # user info: number of guesses/available
     #   guesser or word provider
     @guesses = 0
@@ -37,18 +37,61 @@ class Guess
 
   # full_word_guess
   def word_guess(word)
-  	if word == @correct_word
-  		p "success! well done"
-  	else
-  		p "Noop. Are you feeling alright?"
+  	if word != @correct_word
+      p "Noop. Are you feeling alright?"
+    else
+      p "Success! Well done"
   	end
   end
-
 end
 
 # # GAME BEHAVIOR
 
+puts "\e[H\e[2J"
+p 'Welcome to The Word Guess Game!'
+p 'Guest 1, please cover your eyes.'
 # user 1 starts game by entering a word
+p 'Guest 2, please enter a word then press \'enter.'
+correct_word = gets.chomp
+
+our_game = Guess.new(correct_word)
+puts "\e[H\e[2J"
+
 # user 2 gets so many turns at guessing the letters and/or word
+p 'Okay! Guest 1, you have ' + our_game.number_of_guesses.to_s + ' letters to choose to help guess the word. Please enter a single letter then press \'enter\' to start: '
+player_guess = gets.chomp
 # word is updated with letters and displayed at each guess with ______ denoting length (also number of possible guesses)
 #   correct letters by word length? in order to force word guess ?
+  
+game_status = true
+
+
+while true
+  
+
+  if player_guess.length > 1
+    our_game.word_guess(player_guess)
+    break 
+
+  elsif our_game.guesses == our_game.number_of_guesses
+    p "Sorry, you've run out of guesses!"
+    break
+
+  elsif our_game.guesses < our_game.number_of_guesses && player_guess.length == 1
+    our_game.letter_guess(player_guess)
+
+    puts "\e[H\e[2J"
+    p 'Here is your word hint: ' + our_game.in_progress_word
+    p 'You have ' + (our_game.number_of_guesses - our_game.guesses).to_s + ' letters left.'
+    if (our_game.number_of_guesses - our_game.guesses) == 0
+      p 'Please try and guess the word!:'
+    else
+      p 'Please enter another letter or the whole word: '
+    end
+    player_guess = gets.chomp
+
+
+
+  end
+end
+p "Thanks for playing!"
