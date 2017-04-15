51ff99d3521d16e2664a20c9146e59add4c2ae67
blanklist <joehineriv@gmail.com>
2017-04-15 15:00:15 -0700

    complete first draft

--- ruby/word_game/wheel.rb
+++ ruby/word_game/wheel.rb
@@ -7,7 +7,7 @@ class Guess
     # correct_word = [].join('')
 		@correct_word = correct_word
     # in_progress_word = []
-		@in_progress_word = "_" * correct_word.length
+		@in_progress_word = ("_ " * correct_word.length).strip
     # number of guesses
 		@number_of_guesses = (correct_word.length * 1.5).to_i
     # user info: number of guesses/available
@@ -23,15 +23,17 @@ class Guess
 
   # single_letter_guess
   def letter_guess(current_guess)
-    i = 0
+    x = 0
+    y = 0
     # "repeated guesses do not count against the user"
     @guesses += 1 unless @already_guessed.include?(current_guess)
     @already_guessed << current_guess
-    while i < @correct_word.length
-    	if @correct_word[i] == current_guess
-    		@in_progress_word[i] = current_guess
+    while x < @correct_word.length
+    	if @correct_word[x] == current_guess
+    		@in_progress_word[y] = current_guess
     	end
-    	i += 1
+    	x += 1
+      y += 2
     end
   end
 
@@ -39,59 +41,53 @@ class Guess
   def word_guess(word)
   	if word != @correct_word
       p "Noop. Are you feeling alright?"
-    else
+    elsif word == @correct_word
       p "Success! Well done!"
   	end
   end
 end
-
+  
 # # GAME BEHAVIOR
 
 puts "\e[H\e[2J"
-p 'Welcome to The Word Guess Game!'
-p 'Guest 1, please cover your eyes.'
+puts 'Welcome to The Word Guess Game!'
+puts 'Guest 1, please cover your eyes.'
 # user 1 starts game by entering a word
-p 'Guest 2, please enter a word then press *enter*.'
+puts 'Guest 2, please enter a word then press "enter":'
 correct_word = gets.chomp
 
 our_game = Guess.new(correct_word)
 puts "\e[H\e[2J"
 
 # user 2 gets so many turns at guessing the letters and/or word
-p 'Okay! Guest 1, you have ' + our_game.number_of_guesses.to_s + ' letters to choose to help guess the word. Please enter a single letter then press *enter* to start: '
+puts 'Okay! Guest 1, please choose up to ' + our_game.number_of_guesses.to_s + ' letters to help uncover the mystery word. Enter a single letter then press "enter" to start: '
 player_guess = gets.chomp
-# word is updated with letters and displayed at each guess with ______ denoting length (also number of possible guesses)
-#   correct letters by word length? in order to force word guess ?
-  
-game_status = true
 
-
+# word is updated with letters and displayed at each guess with _ _ _ _ _ _ denoting length (also number of possible guesses)
+#   correct letters by word length? in order to force word guess ?  
 while true
   
-
   if player_guess.length > 1
     our_game.word_guess(player_guess)
     break 
 
   elsif our_game.guesses == our_game.number_of_guesses
-    p "Sorry, you've run out of guesses!"
+    puts "Sorry, you've run out of guesses!"
     break
 
   elsif our_game.guesses < our_game.number_of_guesses && player_guess.length == 1
     our_game.letter_guess(player_guess)
 
     puts "\e[H\e[2J"
-    p 'Here is your word hint: ' + our_game.in_progress_word
-    p 'You have ' + (our_game.number_of_guesses - our_game.guesses).to_s + ' letters left.'
+    puts 'Here is your word hint: ' + our_game.in_progress_word
+    puts 'You have ' + (our_game.number_of_guesses - our_game.guesses).to_s + ' letters left.'
     if (our_game.number_of_guesses - our_game.guesses) == 0
-      p 'Please try and guess the word!:'
+      puts 'Please try and guess the word!:'
     else
-      p 'Please enter another letter or the whole word: '
+      puts 'Please enter another letter or the whole word: '
     end
     player_guess = gets.chomp
 
-
-
   end
 end
-p "Thanks for playing!"
+puts "Thanks for playing!"
--- ruby/word_game/wheel_spec.rb
+++ ruby/word_game/wheel_spec.rb
@@ -4,19 +4,19 @@ describe Guess do
 	let(:current_game) { Guess.new("example") }
 
 	it "stores the correct word, in-progress word, and number of guesses upon initialization" do
-    expect(current_game.output_status).to include("example", "_______", 14)
+    expect(current_game.output_status).to include("example", "_ _ _ _ _ _ _", 10)
 	end
 
   it "updates in-progress word with every letter guess" do
   	current_game.letter_guess("e")
-  	expect(current_game.in_progress_word).to eq("e_____e")
+  	expect(current_game.in_progress_word).to eq("e _ _ _ _ _ e")
     current_game.letter_guess("m")
-  	expect(current_game.in_progress_word).to eq("e__m__e")
+  	expect(current_game.in_progress_word).to eq("e _ _ m _ _ e")
   end
 
   it "checks user word guess against actual word and returns success or failure" do
-    expect(current_game.word_guess("example")).to eq("success! well done")
-    expect(current_game.word_guess("rudabega")).to eq("Noop. Are you feeling alright?")
+    expect(current_game.word_guess("rudabega")).to eq('Noop. Are you feeling alright?')
+    expect(current_game.word_guess("example")).to eq('Success! Well done!')
   end
 	
 end
