4a1e5a0f4b44d1eff252640df72eff340bc43d7b
blanklist <joehineriv@gmail.com>
2017-04-18 18:28:01 -0700

    I might ditch this implementation

--- js/algos.js
+++ js/algos.js
@@ -24,33 +24,35 @@ function findLongest(arraryofStrings) {
   // return true iff value match
   // return false otherwise
 
-
-
-var obj1 = {name: "Dan", age: 30}
-var obj2 = {name: "Joe", age: 37}
+var obj1 = {name: "Dan", something: 3444, age: 30, name: "Bill", else: 1234}
+var obj2 = {name: "Joe", age: 44, else: 1234, something: 3444}
 
 // console.log("age" in obj2 && "age" in obj1)
 
-for (key in obj1) {
-	console.log(obj1[key] === obj2[key]);
+var matchKeys = []
+for (var obj1key in obj1) {
+	for (var obj2key in obj2) {
+		if (obj1key == obj2key) {
+			matchKeys.push(obj1key)
+		}
+	}	
 }
 
+console.log(matchKeys)
 
+function matcher(){//(obj1, obj2, matchKeys) {
+	for (i=0; i<[obj1, obj2].max; i++) {
+		for (j=0; j<matchKeys.length; j++) {
+			if (obj1[matchKeys[i]] == obj2[matchKeys[j]]) {
+				return true
+			} else {
+				return false
+			}
+		}
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(matcher())
 
 // DRIVER CODE
 
