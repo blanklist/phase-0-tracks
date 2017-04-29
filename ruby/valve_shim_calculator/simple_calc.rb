81a850ab99d6e3c5dfb7dece099725f17516a177
blanklist <joehineriv@gmail.com>
2017-04-29 14:57:58 -0700

    complete 8.4

---
+++ ruby/valve_shim_calculator/simple_calc.rb
@@ -0,0 +1,18 @@
+# prompt user for intake or exhaust
+  # 
+  # expand to position
+
+
+
+# prompt user for shim size
+
+# prompt user for clearance
+
+# calculate and return ideal shim size
+
+
+
+
+
+
+# display past calculations
