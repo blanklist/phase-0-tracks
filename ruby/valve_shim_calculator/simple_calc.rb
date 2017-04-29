e6794dc94340c30a633e845ac8b5d390439862e5
blanklist <joehineriv@gmail.com>
2017-04-29 15:13:40 -0700

    user interface pseudocode

--- ruby/valve_shim_calculator/simple_calc.rb
+++ ruby/valve_shim_calculator/simple_calc.rb
@@ -1,10 +1,14 @@
-# prompt user for intake or exhaust
-
-# prompt user for shim size
+# prompt user for intake or exhaust and cylinder position
+	# "Please input cylinder position and intake and exhaust using the format: '1a int' or '3b exh'"
 
 # prompt user for clearance
+  # "what is the clearance at [location]"
+
+# prompt user for shim size
+	# "what is the shim size at [location]"
 
 # calculate and return ideal shim size
+	# "this is the ideal shim size for [location] based on this calculation: [calculation]"
 
 
 
@@ -12,3 +16,4 @@
 
 
 # display past calculations
+	# this may be out of scope for this project but at least a data dump available.
