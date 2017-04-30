4c0bbdfa1b741ff4a74764b082b182eed0091062
blanklist <joehineriv@gmail.com>
2017-04-30 08:34:21 -0700

    dummy table complete, attempting to ignore existing values

--- ruby/valve_shim_calculator/simple_calc.rb
+++ ruby/valve_shim_calculator/simple_calc.rb
@@ -1,12 +1,64 @@
-# build dummy tables
+## VALVE SHIM CALCULATOR ##
+
+require 'sqlite3'
+
+## BUILD DUMMY TABLES ##
+
+valve_calculator_db = SQLite3::Database.new("calculator.db")
+create_measurement_table = <<-measurements
+	CREATE TABLE IF NOT EXISTS measurements(
+		id INTEGER PRIMARY KEY,
+		clr INT,
+		shim INT,
+		ideal INT,
+		cyl_id INT,
+		int_exh_id INT,
+		FOREIGN KEY (cyl_id) REFERENCES cyl(id),
+		FOREIGN KEY (int_exh_id) REFERENCES int_exh(id)
+	)
+measurements
+
+create_cyl_table = <<-cylinder
+	CREATE TABLE IF NOT EXISTS cyl(
+		id INTEGER PRIMARY KEY,
+		cyl VARCHAR(255)
+	)
+cylinder
+
+create_int_exh_table = <<-int_exh
+	CREATE TABLE IF NOT EXISTS int_exh(
+	  id INTEGER PRIMARY KEY,
+	  int_exh VARCHAR(255)
+	)
+int_exh
+
+valve_calculator_db.execute(create_measurement_table)
+valve_calculator_db.execute(create_cyl_table)
+valve_calculator_db.execute(create_int_exh_table)
+
+valve_calculator_db.execute("INSERT OR IGNORE INTO int_exh (int_exh) SELECT ('int') WHERE NOT EXISTS (SELECT 1 FROM int_exh WHERE int_exh = 'int')")
+valve_calculator_db.execute("INSERT OR IGNORE INTO int_exh (int_exh) SELECT ('exh') WHERE NOT EXISTS (SELECT 1 FROM int_exh WHERE int_exh = 'exh')")
+
+cylinder_arr = ['2a', '1a', '2b', '1b', '4a', '3a', '4b', '3b']
+cylinder_arr.each_with_index do |position, index|
+	valve_calculator_db.execute("INSERT INTO cyl (cyl) VALUES ('#{position}')")
+	if position == '2a' || position == '2b' || position == '4a' || position == '4b'
+		valve_calculator_db.execute("INSERT INTO measurements (clr, shim, ideal, cyl_id, int_exh_id) VALUES (0, 0, 0, #{index} + 1, 2)")
+		valve_calculator_db.execute("INSERT INTO measurements (clr, shim, ideal, cyl_id, int_exh_id) VALUES (0, 0, 0, #{index} + 1, 1)")
+	elsif position == '1a' || position == '1b' || position == '3a' || position == '3b'
+		valve_calculator_db.execute("INSERT INTO measurements (clr, shim, ideal, cyl_id, int_exh_id) VALUES (0, 0, 0, #{index} + 1, 1)")
+		valve_calculator_db.execute("INSERT INTO measurements (clr, shim, ideal, cyl_id, int_exh_id) VALUES (0, 0, 0, #{index} + 1, 2)")
+	end	
+end
 
 
-
-
-
+## USER INTERACTION ##
 
 # prompt user for intake or exhaust and cylinder position
 	# "Please input cylinder position and intake and exhaust using the format: '1a int' or '3b exh'"
+
+
+
 
 # prompt user for clearance
   # "what is the clearance at [location]"
