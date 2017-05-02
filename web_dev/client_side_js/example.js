dfb4a4d4209485f55003fac174da26261e92e28d
blanklist <joehineriv@gmail.com>
2017-05-02 11:45:00 -0700

    have made changes with js file

---
+++ web_dev/client_side_js/example.js
@@ -0,0 +1,6 @@
+titles = document.getElementsByTagName('h1');
+title = titles[0];
+title.style.color = "white";
+titles2 = document.getElementsByTagName('h2');
+title2 = titles2[0];
+title2.style.color = "pink";
