851e5514086e0d01d6401663ebae7310aa5d11f8
blanklist <joehineriv@gmail.com>
2017-05-02 12:06:51 -0700

    complete release1

--- web_dev/client_side_js/example.js
+++ web_dev/client_side_js/example.js
@@ -1,6 +1,11 @@
-titles = document.getElementsByTagName('h1');
-title = titles[0];
+var titles = document.getElementsByTagName('h1');
+var title = titles[0];
 title.style.color = "white";
-titles2 = document.getElementsByTagName('h2');
-title2 = titles2[0];
+var titles2 = document.getElementsByTagName('h2');
+var title2 = titles2[0];
 title2.style.color = "pink";
+
+var list = document.getElementsByClassName("column");
+for (var i=0; i<list.length; i++) {
+	list[i].style.color = "blue";
+}
