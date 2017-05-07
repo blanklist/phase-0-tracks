4de1fb6783ed277ae9cede9d48b0c9e7c052381c
blanklist <joehineriv@gmail.com>
2017-05-07 14:48:47 -0700

    working table, form up but not functioning

--- web_dev/valve_sinatra/app.rb
+++ web_dev/valve_sinatra/app.rb
@@ -2,10 +2,16 @@ require 'sinatra'
 require 'sqlite3'
 
 valve_db = SQLite3::Database.new("calculator.db")
-valve_db.results_as_hash = true
 
 get '/' do
-	@values = valve_db.execute("SELECT * FROM calculator")
+	@values = valve_db.execute("SELECT * FROM measurements")
+	@clearance = valve_db.execute("SELECT clr FROM measurements")
+	@shim = valve_db.execute("SELECT shim FROM measurements")
+	@ideal = valve_db.execute("SELECT ideal FROM measurements")
 	erb :home
 end
 
+post '/update' do
+	valve_db.execute("UPDATE measurements SET clr=? WHERE cylinder=?", [params['clearance'], params['cylinder']])
+
+end
